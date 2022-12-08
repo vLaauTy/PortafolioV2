75148e520ee3ba3082cd0b4bf0d54eba65a33839
vLaauTy <96405301+vLaauTy@users.noreply.github.com>
2022-12-08 16:35:26 -0300

    agregado de proyecto

--- src/data/dataProyectosFull.jsx
+++ src/data/dataProyectosFull.jsx
@@ -1,42 +1,52 @@
 export const dataProyectos = [
-    {
-      id: 1,
-      nombre: "TopDrinks",
-      img: "./assets/topDrinks.png",
-      texto:
-        "Pagina realizada simulando un ecommerce de bebidas utilizando useContext y firebase para su base de datos",
-      linkDemo: "https://topdrinks-e8419.firebaseapp.com/",
-      linkRepo: "https://github.com/vLaauTy/topDrinkAtencio",
-      tecnologias: ["ReactJS", "useContext", "Firebase",'MaterialUI'],
-    },
-    {
-      id: 2,
-      nombre: "Hexa Technology",
-      img: "./assets/Hexa.png",
-      texto:
-        "Pagina realizada para la venta de equipos de laboratorio, realizada en equipo junto a un front end developer y dos diseñadoras UX/UI",
-      linkDemo: "https://prueba-hexatech-web.web.app/",
-      linkRepo: "https://github.com/vLaauTy/hexatech.ar",
-      tecnologias: ["ReactJS", "Styled Components","React hook forms","Framer Motion"],
-    },
-    {
-      id: 3,
-      nombre: "ecommerce javascript",
-      img: "./assets/javascript.jpg",
-      texto:
-        "Pagina realizada para practicar codigo javascript",
-      linkDemo: "https://vlaauty.github.io/proyecto_javascript/",
-      linkRepo: "https://github.com/vLaauTy/proyecto_javascript",
-      tecnologias: ["Javascript", "Toastify"],
-    },
-    {
-      id: 4,
-      nombre: "Practica Redux",
-      img: "./assets/redux-practica.png",
-      texto:
-        "Aplicacion para realizar practica en redux-toolkit",
-      linkDemo: "https://redux-toolkit-tasks.web.app/",
-      linkRepo: "https://github.com/vLaauTy/redux-crud",
-      tecnologias: ["ReactJS", "Redux"],
-    },
-  ];
+  {
+    id: 1,
+    nombre: "TopDrinks",
+    img: "./assets/topDrinks.png",
+    texto:
+      "Pagina realizada simulando un ecommerce de bebidas utilizando useContext y firebase para su base de datos",
+    linkDemo: "https://topdrinks-e8419.firebaseapp.com/",
+    linkRepo: "https://github.com/vLaauTy/topDrinkAtencio",
+    tecnologias: ["ReactJS", "useContext", "Firebase", 'MaterialUI'],
+  },
+  {
+    id: 2,
+    nombre: "Hexa Technology",
+    img: "./assets/Hexa.png",
+    texto:
+      "Pagina realizada para la venta de equipos de laboratorio, realizada en equipo junto a un front end developer y dos diseñadoras UX/UI",
+    linkDemo: "https://prueba-hexatech-web.web.app/",
+    linkRepo: "https://github.com/vLaauTy/hexatech.ar",
+    tecnologias: ["ReactJS", "Styled Components", "React hook forms", "Framer Motion"],
+  },
+  {
+    id: 3,
+    nombre: "ecommerce javascript",
+    img: "./assets/javascript.jpg",
+    texto:
+      "Pagina realizada para practicar codigo javascript",
+    linkDemo: "https://vlaauty.github.io/proyecto_javascript/",
+    linkRepo: "https://github.com/vLaauTy/proyecto_javascript",
+    tecnologias: ["Javascript", "Toastify"],
+  },
+  {
+    id: 4,
+    nombre: "Practica Redux",
+    img: "./assets/redux-practica.png",
+    texto:
+      "Aplicacion para realizar practica en redux-toolkit",
+    linkDemo: "https://redux-toolkit-tasks.web.app/",
+    linkRepo: "https://github.com/vLaauTy/redux-crud",
+    tecnologias: ["ReactJS", "Redux"],
+  },
+  {
+    id: 5,
+    nombre: "Pharmacy Medicamentos",
+    img: "./assets/pharmacy-mern.png",
+    texto:
+      "Proyecto realizado con stack mern para resolver necesidades medicas de las personas",
+    linkDemo: "https://pharmacy-mern.onrender.com/",
+    linkRepo: " https://github.com/No-Country/C8-65-FT-MERN",
+    tecnologias: ["MERN", "Tailwind", "Framer Motion"],
+  },
+];
--- src/data/dataProyectosMain.jsx
+++ src/data/dataProyectosMain.jsx
@@ -1,22 +1,22 @@
 export const dataProyectosMain = [
-    {
-      id: 1,
-      nombre: "TopDrinks",
-      img: "./assets/topDrinks.png",
-      texto:
-        "Pagina realizada simulando un ecommerce de bebidas utilizando useContext y firebase para su base de datos",
-      linkDemo: "https://topdrinks-e8419.firebaseapp.com/",
-      linkRepo: "https://github.com/vLaauTy/topDrinkAtencio",
-      tecnologias: ["ReactJS", "useContext", "Firebase",'MaterialUI'],
-    },
-    {
-      id: 2,
-      nombre: "Hexa Technology",
-      img: "./assets/Hexa.png",
-      texto:
-        "Pagina realizada para la venta de equipos de laboratorio, realizada en equipo junto a un front end developer y dos diseñadoras UX/UI",
-      linkDemo: "https://prueba-hexatech-web.web.app/",
-      linkRepo: "https://github.com/vLaauTy/hexatech.ar",
-      tecnologias: ["ReactJS", "Styled Components","React hook forms","Framer Motion"],
-    },
-  ];
+  {
+    id: 1,
+    nombre: "Pharmacy Medicamentos",
+    img: "./assets/pharmacy-mern.png",
+    texto:
+      "Proyecto realizado con stack mern para resolver necesidades medicas de las personas",
+    linkDemo: "https://pharmacy-mern.onrender.com/",
+    linkRepo: " https://github.com/No-Country/C8-65-FT-MERN",
+    tecnologias: ["MERN", "Tailwind", "Framer Motion"],
+  },
+  {
+    id: 2,
+    nombre: "Hexa Technology",
+    img: "./assets/Hexa.png",
+    texto:
+      "Pagina realizada para la venta de equipos de laboratorio, realizada en equipo junto a un front end developer y dos diseñadoras UX/UI",
+    linkDemo: "https://prueba-hexatech-web.web.app/",
+    linkRepo: "https://github.com/vLaauTy/hexatech.ar",
+    tecnologias: ["ReactJS", "Styled Components", "React hook forms", "Framer Motion"],
+  },
+];
